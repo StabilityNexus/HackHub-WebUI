1aaa604f29a289226b8bacdc670d6ef4dec8e933
DengreSarthak <dengresarthak2002@gmail.com>
2025-08-29 16:24:05 +0530

    Abi updated

--- utils/contractAddress.ts
+++ utils/contractAddress.ts
@@ -1,5 +1,5 @@
 export const HackHubFactoryAddress: { [key: number]: `0x${string}` } = {
-  534351: '0x8b5f09679b9a09992eebc0a5d13dc955d9864cf8',
+  534351: '0x451b43037c2224493ace56865a87a9cb17540474',
 }
 
 export const getFactoryAddress = (chainId: number): `0x${string}` | undefined => {
