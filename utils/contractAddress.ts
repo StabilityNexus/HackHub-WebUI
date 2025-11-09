411a05c72a2978bc624eda2340b0ad53e9c17f9c
DengreSarthak <dengresarthak2002@gmail.com>
2025-11-09 13:05:19 +0530

    Deployed on Polygon

--- utils/contractAddress.ts
+++ utils/contractAddress.ts
@@ -1,5 +1,6 @@
 export const HackHubFactoryAddress: { [key: number]: `0x${string}` } = {
   534351: '0x451b43037c2224493ace56865a87a9cb17540474',
+  137: '0x27732c89F81bbE95a8B75e05Ba0ff90A24ecd8Aa'
 }
 
 export const getFactoryAddress = (chainId: number): `0x${string}` | undefined => {
